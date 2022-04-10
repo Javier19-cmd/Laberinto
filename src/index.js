5f72647b0f6a3763f40c0446215d889075e6a43e
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-04-09 20:50:48 -0600

    agregando cosas del webpack

---
+++ src/index.js
@@ -0,0 +1,19 @@
+/* 
+Instalaciones realizadas: 
+
+1. Webpack: npm install webpack webpack-cli.
+2. 
+*/
+
+//import Header from './components/Header' //Importando el módulo de Header.
+import {sum, sub} from './utils.js'
+
+console.log('Hello from index.js')
+
+console.log('sum 2 + 2', sum(2,2)) //Suma de dos elementos.
+
+console.log('sub 2 - 2', sub(2,2)) //Resta de dos elementos.
+
+//const root = document.getElementById('root') //Accediento al root del html.
+
+//Header(root, 'Hello world') //Mandando al root el hello world.
