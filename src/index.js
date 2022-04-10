dacb4b77af6bf425f9bc4e39fedb3cc245734666
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-04-09 21:03:53 -0600

    :v:

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ Otras observaciones:
 1. En el .gitignore se puso el node_modules, que nunca se tiene que subir.
 2. Se escribió el npm run build para que se pueda correr en el navegador.
 3. Para generar el archivo main.js en la carpeta dist, se puso en el package.json y en su apartado de scripts la instrucción "build": "webpack --mode development"
-y se ejecutó el comando npm run build. En el .gitignore se puso que no se tiene que subir el main.js.
+y se ejecutó el comando npm run build. En el .gitignore se puso que no se tiene que subir el main.js. El "build": "webpack --mode development" es para programar.
 4. Para recuperar el main.js se tiene que correr el npm run build.
-5. El "build": "webpack --mode production" es lo que se va para los clientes.
+5. El "build": "webpack --mode production" es lo que se va para los clientes. El "build": "webpack --mode production" es para producción final de la página.
 */
 
 //import Header from './components/Header' //Importando el módulo de Header.
