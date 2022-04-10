fa709cd5cf20af29caea0e04d3754795ce06310e
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-04-10 01:29:22 -0600

    Separando cosas
    Separando el axios a un componente individual y separando sus propiedades para una mejor manipulación

---
+++ src/components/Laberinto.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import styles from './Laberinto.css'
+
+function Laberinto () {
+    
+    //Api: https://maze.juanelcaballo.club/?type=json&w=4&h=4.
+
+    //useState sirve para modificar el estado de un componente.
+    const [laberinto, setLaberinto] = useState([]) //Se le pasa un array vacío porque va a ser el estado del maze; la variable laberinto va a ser el estado inicial del maze.
+                                                    //setLaberinto permite actualizar el estado del laberinto.
+    
+    console.log(laberinto)
+
+    //useEffect sirve para poder ver efectos secundarios en componentes.
+    useEffect(() => {
+
+        //Función que manda a pedir el laberinto al API. Esta función va a hacer un pedido asincrónico. Va a tener un async y un await. 
+        //El async determina que la función va a tener un pedido asincrónico y cuando se hace el pedido, no se sabe cuánto va a tardar. En ese momento se pone un await
+        //para decirle que espere hasta que se haga la consulta.
+        const obtenerLaberinto = async() => {
+             const url = 'https://maze.juanelcaballo.club/?type=json&w=4&h=4' //Url del API. 
+             const result = await axios.get(url) //Pedido con axios y get. Se pone await para que espere la respuesta.
+             //console.log(result.data) //Imprimiendo la data del API.
+            //Se desea guardar en actores.
+            //La función para actualizar actores es setLaberinto.
+            setLaberinto(result.data) //Se llama a setLaberinto y se le pasa como parámetro el result.data que es la matriz de que se pidió desde el API.
+        }
+
+        obtenerLaberinto() //Poniendo a funcionar la promesa.
+
+
+    }, []) //Si no se pone un array vacío, entonces la función se ejecuta en un loop infinito.
+
+    return(
+        <div className='Laberinto'> {/*Padre de todos los div's*/}
+            {/*Devolviendo un map que va a imprimir cada elemento del laberinto*/}
+            Imprimiendo el laberinto
+            {!laberinto ? 'Cargando...' : laberinto.map((laberinto,index) => { {/*Mapeando el laberinto*/}
+                return <div key={index} className="Objetos">{laberinto}</div> /*Se está retornando como div el laberinto*/
+            })}
+        </div>
+    )
+
+    //console.log(laberinto) //Imprimiendo el array del laberinto.
+}
+
+export default Laberinto
--- src/index.js
+++ src/index.js
@@ -17,61 +17,20 @@ Otras observaciones:
 y se ejecutó el comando npm run build. En el .gitignore se puso que no se tiene que subir el main.js. El "build": "webpack --mode development" es para programar.
 4. Para recuperar el main.js se tiene que correr el npm run build.
 5. El "build": "webpack --mode production" es lo que se va para los clientes. El "build": "webpack --mode production" es para producción final de la página.
+
+Referencias: 
+1. https://www.youtube.com/watch?v=R5LTmCSOVCg&ab_channel=FacundoTissera
+
 */
 
-import Header from './components/Header.jsx' //Importando el módulo de Header.
-import {sum, sub} from './utils.js'
+//import Header from './components/Header.jsx' //Importando el módulo de Header.
+//import {sum, sub} from './utils.js'
 import React, { useState, useEffect } from 'react'
 //import ReactDOM from 'react-dom'
 import * as ReactDOMClient from 'react-dom/client' //Importando el ReactDOMClient.
-import css from './style.css' 
-import axios from 'axios'
+import styles from './style.css' 
+import Laberinto from './components/Laberinto.js'
 
-function Laberinto () {
-    
-    //Api: https://maze.juanelcaballo.club/?type=json&w=4&h=4.
-
-    //useState sirve para modificar el estado de un componente.
-    const [laberinto, setLaberinto] = useState([]) //Se le pasa un array vacío porque va a ser el estado del maze; la variable laberinto va a ser el estado inicial del maze.
-                                                    //setLaberinto permite actualizar el estado del laberinto.
-    
-    console.log(laberinto)
-
-    //useEffect sirve para poder ver efectos secundarios en componentes.
-    useEffect(() => {
-
-        //Función que manda a pedir el laberinto al API. Esta función va a hacer un pedido asincrónico. Va a tener un async y un await. 
-        //El async determina que la función va a tener un pedido asincrónico y cuando se hace el pedido, no se sabe cuánto va a tardar. En ese momento se pone un await
-        //para decirle que espere hasta que se haga la consulta.
-        const obtenerLaberinto = async() => {
-             const url = 'https://maze.juanelcaballo.club/?type=json&w=4&h=4' //Url del API. 
-             const result = await axios.get(url) //Pedido con axios y get. Se pone await para que espere la respuesta.
-             //console.log(result.data) //Imprimiendo la data del API.
-            //Se desea guardar en actores.
-            //La función para actualizar actores es setLaberinto.
-            setLaberinto(result.data) //Se llama a setLaberinto y se le pasa como parámetro el result.data que es la matriz de que se pidió desde el API.
-        }
-
-        obtenerLaberinto() //Poniendo a funcionar la promesa.
-
-
-    }, []) //Si no se pone un array vacío, entonces la función se ejecuta en un loop infinito.
-
-    return(
-        <div>
-            {laberinto.map(function(objeto, i) { 
-                
-                console.log(objeto)
-                
-               return <div key={i}>{objeto}</div>
-                
-            }
-            )}
-        </div>
-    )
-
-    //console.log(laberinto) //Imprimiendo el array del laberinto.
-}
 
 const App = () => {
 
