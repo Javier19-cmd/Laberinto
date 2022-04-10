f52d0e07cf0e14801c8bbc8d58bbf235c61fe8eb
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-04-10 17:25:41 -0600

    :v:

--- src/components/Laberinto.js
+++ src/components/Laberinto.js
@@ -10,7 +10,7 @@ function Laberinto () {
     const [laberinto, setLaberinto] = useState([]) //Se le pasa un array vacío porque va a ser el estado del maze; la variable laberinto va a ser el estado inicial del maze.
                                                     //setLaberinto permite actualizar el estado del laberinto.
     
-    console.log(laberinto)
+    //console.log(laberinto)
 
     //useEffect sirve para poder ver efectos secundarios en componentes.
     useEffect(() => {
@@ -20,11 +20,14 @@ function Laberinto () {
         //para decirle que espere hasta que se haga la consulta.
         const obtenerLaberinto = async() => {
              const url = 'https://maze.juanelcaballo.club/?type=json&w=5&h=5' //Url del API. 
-             const result = await axios.get(url) //Pedido con axios y get. Se pone await para que espere la respuesta.
+             const result = await fetch(url) //Pedido con axios y get. Se pone await para que espere la respuesta.
+             const resultado = await result.json()
              //console.log(result.data) //Imprimiendo la data del API.
+             console.log(resultado)
             //Se desea guardar en actores.
             //La función para actualizar actores es setLaberinto.
-            setLaberinto(result.data) //Se llama a setLaberinto y se le pasa como parámetro el result.data que es la matriz de que se pidió desde el API.
+            setLaberinto(resultado) //Se llama a setLaberinto y se le pasa como parámetro el result.data que es la matriz de que se pidió desde el API.
+            cosas()
         }
 
         obtenerLaberinto() //Poniendo a funcionar la promesa.
@@ -32,21 +35,54 @@ function Laberinto () {
 
     }, []) //Si no se pone un array vacío, entonces la función se ejecuta en un loop infinito.
 
+    //Método para poder ver cada elemento del laberinto.
+    const cosas = () => {
+
+        setLaberinto(prevLaberinto => {
+            return prevLaberinto.map(objeto => {
+                //Con una función de devolución ES5
+                objeto.forEach(function (element) {
+                    
+                    //Elemento player.
+                    if(element === "p"){
+                        console.log("Elemento player", element)
+                        return <div className="Player">{element}</div>
+                        
+                    }
+                    
+                    //Elememento más +.
+                    if(element === "+"){
+                        console.log("Elemento '+'", element)
+                        return <div className="Mas">{element}</div>
+                    }
+
+                    //Elememento menos "-".
+                    if(element === "-"){
+                        console.log("Elemento '-'", element)
+                        return <div className="Menos">{element}</div>
+                    }
+
+                    //Elememento palo "|".
+                    if(element === "-"){
+                        console.log("Elemento '|'", element)
+                        return <div className="Palo">{element}</div>
+                    }
+
+                    //Elememento meta "g".
+                    if(element === "g"){
+                        console.log("Elemento 'g'", element)
+                        return <div className="Meta">{element}</div>
+                    }
+                })
+            })
+        })
+    }
+
     return(
         <div> {/*Padre de todos los div's*/}
             {/*Devolviendo un map que va a imprimir cada elemento del laberinto*/}
             <h2>Imprimiendo el laberinto</h2> {/*Imprimiendo en pantalla un título h2 para indicar que se está imprimiendo el laberinto*/}
-            {laberinto.map((hola, index) => { /*Mapeando el laberinto: Si en caso no hay nada, entonces se dice que se está cargando. Si no, se pasa al map.*/
-                
-                //Devolviendo un div con un hijo h3 que tiene la matriz de lo que se pidió en el API.
-                return(
-                    <div key={index} className='Hola'>
-                        <h3>{index}</h3>
-                        <h3>{hola}</h3>
-                    </div>
-                    )
-            
-            })}
+            <Laberinto/>
         </div>
     )
 
