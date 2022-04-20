7c1303e598de9a0868093c12eff51b1260b2c6fc
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-04-20 10:25:34 -0600

    agregando diseños

--- src/components/Laberinto.js
+++ src/components/Laberinto.js
@@ -42,7 +42,7 @@ function Laberinto () {
     //Api: https://maze.juanelcaballo.club/?type=json&w=4&h=4.
 
     //useState sirve para modificar el estado de un componente.
-    const [laberinto, setLaberinto] = useState([]) //Se le pasa un array vacío porque va a ser el estado del maze; la variable laberinto va a ser el estado inicial del maze.
+    const [maze, setMaze] = useState([]) //Se le pasa un array vacío porque va a ser el estado del maze; la variable laberinto va a ser el estado inicial del maze.
                                                     //setLaberinto permite actualizar el estado del laberinto.
 
     /*Estados para las posiciones x e y*/
@@ -63,11 +63,11 @@ function Laberinto () {
             //Pedido con fetch.
              fetch(url)
                 .then(res => res.json()) 
-                .then(datos => setLaberinto(datos))//Se llama a setLaberinto y se le pasa como parámetro el result.data que es la matriz de que se pidió desde el API.
+                .then(datos => setMaze(datos))//Se llama a setLaberinto y se le pasa como parámetro el result.data que es la matriz de que se pidió desde el API.
              //console.log(result.data) //Imprimiendo la data del API.
              //console.log(resultado) //Imprimiendo las cosas que trajo del API.
              //La función para actualizar elementos es setLaberinto.
-            //console.log(laberinto)
+            //console.log(maze)
         }
 
         obtenerLaberinto() //Poniendo a funcionar la promesa.
@@ -75,48 +75,49 @@ function Laberinto () {
     }, []) //Si no se pone un array vacío, entonces la función se ejecuta en un loop infinito.
 
 
-    console.log(laberinto) //Corroborando la matriz que se trajo del API.
+    console.log(maze) //Corroborando la matriz que se trajo del API.
 
     return(
         <div className="Maze"> {/*Padre de todos los div's*/}
             {/*Devolviendo un map que va a imprimir cada elemento del laberinto*/}
-            <h2>Imprimiendo el laberinto</h2> {/*Imprimiendo en pantalla un título h2 para indicar que se está imprimiendo el laberinto*/}
+            <h2>Ahora trata de llegar a la meta</h2> {/*Imprimiendo en pantalla un título h2 para indicar que se está imprimiendo el laberinto*/}
             <div className='Mapa'> {/*Se mapea la matriz de laberinto y luego se mapea cada matriz que hay dentro de la matriz grande.*/}
                 { /*Jalando todos los objetos de la matriz devuelta por el fetch*/
-                    laberinto.map((elementos) => 
+                    maze.map((elementos) => 
                         /*Entrando a la matriz que se trae desde el API*/
                      <div className='Todo' key={Math.random()}>{
                         elementos.map((elemento) => {
                             /*Leyendo cada elemento de la matriz que se trae desde el API*/
                             if(elemento === "-" || elemento === "|" || elemento === "+"){
                                 /*Si la matriz tiene estos elementos, entonces significa que hay paredes y suelos*/
+                                /*Llamando al elemento Paredes para dibujarlas en la pantalla*/
                                 return <Paredes
                                         key = {Math.random()}
-                                        />  /*Llamando al elemento Paredes para dibujarlas en la pantalla*/
-                            }else if(elemento === " "){
+                                        /> 
+                            }if(elemento === " "){
+                                /*Llamando al elemento otro para dibujar un espacio vacío en el canvas*/
                                 return <Otro 
                                         key={Math.random()}
-                                        />/*Llamando al elemento otro para dibujar un espacio vacío en el canvas*/
-                            }else if(elemento === "p"){
+                                        />
+                            }if(elemento === "p"){
                                 /*Llamando al método de Jugador para que cree un div personalizado*/
                                 return <Jugador
                                     key={Math.random()}
                                 />
-                            }else if(elemento === "g"){
+                            }if(elemento === "g"){
+                                /*Llamando al elemento meta para dibujarla en el canvas*/
                                 return <Meta
                                     key={Math.random()}
                                 />                                
                             }
-                        })
-                        }
+                        })}
                         </div>
-                    )
-                }
+                    )}
             </div>
         </div>
     )
 
-    //console.log(laberinto) //Imprimiendo el array del laberinto.
+    //console.log(maze) //Imprimiendo el array del laberinto.
 }
 
 export default Laberinto
