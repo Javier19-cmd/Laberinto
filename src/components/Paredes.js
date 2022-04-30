9d849ae45077843d0e3abcca8c5cc0165d1b217f
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-04-30 13:21:53 -0600

    arreglando errores de las paredes

--- src/components/Paredes.js
+++ src/components/Paredes.js
@@ -1,18 +1,16 @@
-/*
- Nombre: Javier Valle
- Carnet: 20159 
-*/
+// Nombre: Javier Valle
+// Carnet: 20159
 
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import pared from './pared.svg'
 
-//Función que procesa los elementos y los manda a la pantalla. Este lee a todas las paredes.
+// Función que procesa los elementos y los manda a la pantalla. Este lee a todas las paredes.
 function Paredes(){
-    return(
-        <div className='Pared'>
-            <img className='Paredes' src={pared}/>
-        </div> /*Haciendo div para los elemenetos paredes*/
-    )
+  return (
+    <div className="Pared">
+      <img className="Paredes" src={pared} alt="Pared" />
+    </div> /* Haciendo div para los elemenetos paredes */
+  )
 }
 
 export default Paredes
