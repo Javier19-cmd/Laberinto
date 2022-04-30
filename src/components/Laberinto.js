d8eab18f01022168d218a88ac54da71a70d54b4d
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-04-30 13:59:21 -0600

    arreglando cosas

--- src/components/Laberinto.js
+++ src/components/Laberinto.js
@@ -238,60 +238,54 @@ function Laberinto () {
 
 
     return(
-        <div className="Maze"> {/*Padre de todos los div's*/}
-            {/*Devolviendo un map que va a imprimir cada elemento del laberinto*/}
-            <h2>Ahora trata de llegar a la meta</h2> {/*Imprimiendo en pantalla un título h2 para indicar que se está imprimiendo el laberinto*/}
-            {/*Reproductor de audio*/}
-            <div className='Mapa'> {/*Se mapea la matriz de laberinto y luego se mapea cada matriz que hay dentro de la matriz grande.*/}
-                { /*Jalando todos los objetos de la matriz devuelta por el fetch*/
-                    maze.map((elementos) => 
-                        /*Entrando a la matriz que se trae desde el API*/
-                     <div className='Todo' key={Math.random()}>{
-                        elementos.map((elemento) => {
-                            /*Leyendo cada elemento de la matriz que se trae desde el API*/
-                            if(elemento === "-" || elemento === "|" || elemento === "+"){
-                                /*Si la matriz tiene estos elementos, entonces significa que hay paredes y suelos*/
-                                /*Llamando al elemento Paredes para dibujarlas en la pantalla*/
-                                return <Paredes
-                                        key = {Math.random()}
-                                        /> 
-                            }if(elemento === " "){
-                                /*Llamando al elemento otro para dibujar un espacio vacío en el canvas*/
-                                return <Otro 
-                                        key={Math.random()}
-                                        />
-                            }if(elemento === "p"){
-                                return <>
-                                 <Jugador
-                                        key={Math.random()}
-                                        x={parseInt(posx, 10)} /*Se parsea a entero para que no se pase como string*/
-                                        y={parseInt(posy, 10)} /*Se parsea a entero para que no se pase como string*/
-                                    />
-                                    {/*Creando puerta de salida*/}
-                                    <Puerta
-                                        key = {Math.random()}
-                                    />
-                                </>
-                            }
-                            if(elemento === "g"){
-                                /*Llamando al elemento meta para dibujarla en el canvas*/
-                                return <Meta
-                                    key={Math.random()}
-                                />                      
-                            }
-                        })}
-                        </div>
-                    )}     
-            </div>
-            {
-                /*Buscando al jugador*/
-            }
-            
-                
-        </div>
-    )
+      <div className="Maze"> {/*Padre de todos los div's*/}
+          {/*Devolviendo un map que va a imprimir cada elemento del laberinto*/}
+          <h2>Ahora trata de llegar a la meta</h2> {/*Imprimiendo en pantalla un título h2 para indicar que se está imprimiendo el laberinto*/}
+          {/*Reproductor de audio*/}
+          <div className='Mapa'> {/*Se mapea la matriz de laberinto y luego se mapea cada matriz que hay dentro de la matriz grande.*/}
+              { /*Jalando todos los objetos de la matriz devuelta por el fetch*/
+                  maze.map((elementos) => 
+                      /*Entrando a la matriz que se trae desde el API*/
+                    <div className="Todo" key={Math.random()}>{
+                      elementos.map((elemento) => {
+                        /* Leyendo cada elemento de la matriz que se trae desde el API */
+                        if(elemento === "-" || elemento === "|" || elemento === "+") {
+                          //    Detectando paredes y suelos
+                            return <Paredes
+                                    key = {Math.random()}
+                                    /> 
+                          }if(elemento === " "){
+                              /*Llamando al elemento otro para dibujar un espacio vacío en el canvas*/
+                              return <Otro 
+                                      key={Math.random()}
+                                      />
+                          } if(elemento === "p"){
+                              return <>
+                                <Jugador
+                                      key={Math.random()}
+                                      x={parseInt(posx, 10)} /*Se parsea a entero para que no se pase como string*/
+                                      y={parseInt(posy, 10)} /*Se parsea a entero para que no se pase como string*/
+                                  />
+                                  {/*Creando puerta de salida*/}
+                                  <Puerta
+                                      key = {Math.random()}
+                                  />
+                              </>
+                          }
+                          if(elemento === "g"){
+                              /*Llamando al elemento meta para dibujarla en el canvas*/
+                              return <Meta
+                                  key={Math.random()}
+                              />                      
+                          }
+                          return
+                      })}
+                      </div>
+                  )}
 
-    //console.log(maze) //Imprimiendo el array del laberinto.
+          </div>
+      </div>
+  )
 }
 
 export default Laberinto
--- src/index.js
+++ src/index.js
@@ -1,64 +1,25 @@
-/* 
+//  Nombre: Javier Valle
+//  Carnet: 20159
 
-Nombre: Javier Valle
-Carnet: 20159
-
-Instalaciones realizadas: 
-
-1. Webpack: npm install webpack webpack-cli. Para reinstalar node_modules se corre npm install.
-2. React: npm install react react-dom.
-3. Babel: npm install babel-loader @babel/core @babel/preset-react 
-4. Webpack para correr el código: npm install webpack-dev-server
-
-CSS: 
-5. npm install --save-dev style-loader
-6. npm install -D mini-css-extract-plugin
-7. npm install --save-dev css-loader
-
-Audio:
-8. npm install --save react-audio-player
-
-Otras observaciones: 
-1. En el .gitignore se puso el node_modules, que nunca se tiene que subir.
-2. Se escribió el npm run build para que se pueda correr en el navegador.
-3. Para generar el archivo main.js en la carpeta dist, se puso en el package.json y en su apartado de scripts la instrucción "build": "webpack --mode development"
-y se ejecutó el comando npm run build. En el .gitignore se puso que no se tiene que subir el main.js. El "build": "webpack --mode development" es para programar.
-4. Para recuperar el main.js se tiene que correr el npm run build.
-5. El "build": "webpack --mode production" es lo que se va para los clientes. El "build": "webpack --mode production" es para producción final de la página.
-
-Referencias: 
-1. https://www.youtube.com/watch?v=R5LTmCSOVCg&ab_channel=FacundoTissera
-
-*/
-import React, { useState, useEffect } from 'react'
-//import ReactDOM from 'react-dom'
-import * as ReactDOMClient from 'react-dom/client' //Importando el ReactDOMClient.
-import styles from './style.css' 
+import React from 'react'
+import * as ReactDOMClient from 'react-dom/client' //  Importando el ReactDOMClient.
 import Laberinto from './components/Laberinto.js'
 
-const App = () => {
-    return(
-        <>
-            <h1>Laberinto</h1>
-            <Laberinto/>
-        </>
-    )
+function App() {
+  return (
+    <div>
+      <h1>Laberinto</h1>
+      <Laberinto />
+    </div>
+  )
 }
 
-
-//Creando la raíz del código.
+// Creando la raíz del código.
 const root = ReactDOMClient.createRoot(
-    document.getElementById('root')
-  );
-  
-  //Enlazado
-  root.render(
-    <App/>
-  )
+  document.getElementById('root'),
+)
 
-/*
-ReactDOM.render(
-    <App/>,
-    document.getElementById('root')
-    
-)*/
+//  Enlazado
+root.render(
+  <App />,
+)
