740e90308b1ba2c535f7e675942ca538e30d585e
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-04-30 13:26:26 -0600

    arreglando y quitando cosas

--- src/components/Puerta.js
+++ src/components/Puerta.js
@@ -1,18 +1,16 @@
-/*
- Nombre: Javier Valle
- Carnet: 20159 
-*/
+// Nombre: Javier Valle
+// Carnet: 20159
 
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import Puertas from './puerta.jpg'
 
-//Función que procesa los elementos y los manda a la pantalla. Este lee el espacio vacío.
-function Puerta(){
-    return(
-        <div className='Puerta'>
-            <img className='Puertas' src={Puertas}/>
-        </div> /*Haciendo div para el espacio vacío*/
-    )
+// Función que procesa los elementos y los manda a la pantalla. Este lee el espacio vacío.
+function Puerta() {
+  return (
+    <div className="Puerta">
+      <img className="Puertas" src={Puertas} alt="Puerta" />
+    </div> /* Haciendo div para el espacio vacío */
+  )
 }
 
 export default Puerta
