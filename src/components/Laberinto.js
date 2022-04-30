f9aac34cf0a13247a04fa1db62ed3a0fd48aaa97
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-04-30 14:09:12 -0600

    arreglando cosas

--- src/components/Laberinto.js
+++ src/components/Laberinto.js
@@ -1,7 +1,7 @@
-/*
- Nombre: Javier Valle
- Carnet: 20159 
-*/
+
+//  Nombre: Javier Valle
+//  Carnet: 20159 
+
 
 import React, { useState, useEffect, useRef } from 'react'
 import styles from './Laberinto.css'
@@ -13,209 +13,209 @@ import Puerta from './Puerta.js'
 
 function Laberinto () {
     
-    //API: https://maze.juanelcaballo.club/?type=json&w=4&h=4.
+    //  API: https://maze.juanelcaballo.club/?type=json&w=4&h=4.
 
-    //useState sirve para modificar el estado de un componente.
-    const [maze, setMaze] = useState([]) //Se le pasa un array vacío porque va a ser el estado del maze; la variable laberinto va a ser el estado inicial del maze.
-                                        //setLaberinto permite actualizar el estado del laberinto.
+    //  useState sirve para modificar el estado de un componente.
+    const [maze, setMaze] = useState([]) // Se le pasa un array vacío porque va a ser el estado del maze; la variable laberinto va a ser el estado inicial del maze.
+                                        //  setLaberinto permite actualizar el estado del laberinto.
 
-    /*Estados para las posiciones 'x' y 'y'*/
-    //Se inicializa en 1, dado que el jugador está en (1,1)
+    /*  Estados para las posiciones 'x' y 'y' */
+    //  Se inicializa en 1, dado que el jugador está en (1,1)
     const [posx, setPosx] = useState(75)
     const [posy, setPosy] = useState(75)
 
     console.log(posx)
     
-    //useEffect sirve para poder ver efectos secundarios en componentes.
+    //  useEffect sirve para poder ver efectos secundarios en componentes.
     useEffect(() => {
 
-        //Función que manda a pedir el laberinto al API. 
-        //El async determina que la función va a tener un pedido asincrónico y cuando se hace el pedido, no se sabe cuánto va a tardar. En ese momento se pone un await
-        //para decirle que espere hasta que se haga la consulta.
+        //  Función que manda a pedir el laberinto al API. 
+        //  El async determina que la función va a tener un pedido asincrónico y cuando se hace el pedido, no se sabe cuánto va a tardar. En ese momento se pone un await
+        //  para decirle que espere hasta que se haga la consulta.
         const obtenerLaberinto = () => {
 
-            /*Link del API: https://maze.juanelcaballo.club/?type=json&w=4&h=4*/
+            /*  Link del API: https://maze.juanelcaballo.club/?type=json&w=4&h=4  */
 
-            //Trayendo el laberinto desde el API.
-             const url = 'https://maze.juanelcaballo.club/?type=json&w=4&h=4' //Url del API. 
-            //Pedido con fetch.
+            //  Trayendo el laberinto desde el API.
+             const url = 'https://maze.juanelcaballo.club/?type=json&w=4&h=4' //  Url del API. 
+            //  Pedido con fetch.
              fetch(url)
                 .then(res => res.json()) 
-                .then(datos => setMaze(datos))//Se llama a setMaze y se le pasa como parámetro el result.data que es la matriz de que se pidió desde el API.
-             //console.log(result.data) //Imprimiendo la data del API.
-             //console.log(resultado) //Imprimiendo las cosas que trajo del API.
-             //La función para actualizar elementos es setMaze.
-            //console.log(maze)
+                .then(datos => setMaze(datos))//  Se llama a setMaze y se le pasa como parámetro el result.data que es la matriz de que se pidió desde el API.
+             // console.log(result.data) //Imprimiendo la data del API.
+             // console.log(resultado) //Imprimiendo las cosas que trajo del API.
+             // La función para actualizar elementos es setMaze.
+            //  console.log(maze)
         }
 
         obtenerLaberinto() //Poniendo a funcionar la promesa.
-    }, []) //Si no se pone un array vacío, entonces la función se ejecuta en un loop infinito.
+    }, []) // Si no se pone un array vacío, entonces la función se ejecuta en un loop infinito.
 
    // console.log(maze) //Corroborando la matriz que se trajo del API.
 
-   //console.log("Laberinto antes del event listener: ",maze) //Comprobando el array del laberinto.
+   // console.log("Laberinto antes del event listener: ",maze) //Comprobando el array del laberinto.
 
-   //Función del movimiento.
+   // Función del movimiento.
 
     const mov = React.useCallback((e) =>{
 
         if(e.key === "ArrowLeft"){
-            //setPosx((oldPosx) => oldPosx - 1)
+            //  setPosx((oldPosx) => oldPosx - 1)
             
-            //Posición inicial del personaje.
+            //  Posición inicial del personaje.
 
-            //Menos uno de la posición en x del personaje. 
-            //Posición del personaje en x.
+            //  Menos uno de la posición en x del personaje. 
+            //  Posición del personaje en x.
             const lpixelx = posx - 1
 
             console.log(lpixelx)
-            //Posición del personaje en y.
+            //  Posición del personaje en y.
             const lpixely = posy 
 
-            //Obteniendo la posición del personaje en el maze.
-            const indexX = Math.ceil((lpixelx/75)) //Posición en x.
-            const indexY = Math.ceil((lpixely/75)) //Posición en y.
+            //  Obteniendo la posición del personaje en el maze.
+            const indexX = Math.ceil((lpixelx/75)) // Posición en x.
+            const indexY = Math.ceil((lpixely/75)) // Posición en y.
 
-            ///console.log("Este es el maze cuando se presiona el botón de la izquierda: ", maze)
+            //  console.log("Este es el maze cuando se presiona el botón de la izquierda: ", maze)
 
-            //console.log(maze, indexX, indexY) //Posición del personaje en el maze.
-            //console.log(lpixelx)               //Posición inicial del personaje en x. 
+            //  console.log(maze, indexX, indexY) //  Posición del personaje en el maze.
+            //  console.log(lpixelx)               // Posición inicial del personaje en x. 
 
-            //console.log(lpixely)               //Posición del personaje ne y.
+            //  console.log(lpixely)               // Posición del personaje ne y.
               
-            //console.log("Datos del lab", maze)
+            //  console.log("Datos del lab", maze)
 
-            /*Detectando la flecha izquierda para poder mover al personaje*/
+            /*  Detectando la flecha izquierda para poder mover al personaje  */
             console.log("Izquierda")
         
-            //setPosx(posx - 1) //Haciendo set del movimiento hacia la izquierda
+            //  setPosx(posx - 1) //Haciendo set del movimiento hacia la izquierda
 
-            //setPosx(indexX)
+            //  setPosx(indexX)
 
-            //setPosx((oldPosx) => oldPosx - 1) //Haciendo set del movimiento hacia la derecha.
+            //  setPosx((oldPosx) => oldPosx - 1) //  Haciendo set del movimiento hacia la derecha.
             
-            //Viendo si hay un espacio vacío.
+            //  Viendo si hay un espacio vacío.
             if(maze[indexY][indexX] === ' '){
-              setPosx((oldPosx) => oldPosx - 10) //Moviendo al personaje hacia la izquierda.
+              setPosx((oldPosx) => oldPosx - 10) // Moviendo al personaje hacia la izquierda.
             }else if(maze[indexY][indexX] === 'g'){
                 alert("Ganaste!")
             }
 
         }else if(e.key === "ArrowRight"){
 
-            //Menos uno de la posición en x del personaje. 
-            //Posición del personaje en x.
+            //  Menos uno de la posición en x del personaje. 
+            //  Posición del personaje en x.
             const lpixelx = posx + 1
 
-            //console.log({lpixelx})
-            //Posición del personaje en y.
+            //  console.log({lpixelx})
+            //  Posición del personaje en y.
             const lpixely = posy 
 
-            //Obteniendo la posición del personaje en el maze.
-            const indexX = Math.ceil((lpixelx/75)) //Posición en x.
-            const indexY = Math.ceil((lpixely/75)) //Posición en y.
+            //  Obteniendo la posición del personaje en el maze.
+            const indexX = Math.ceil((lpixelx/75)) // Posición en x.
+            const indexY = Math.ceil((lpixely/75)) // Posición en y.
 
             console.log("Derecha")
 
             
-            /*Detectando la flecha derecha para poder mover al personaje*/
-            //console.log("Derecha", indexX, indexY)
+            /*  Detectando la flecha derecha para poder mover al personaje  */
+            //  console.log("Derecha", indexX, indexY)
 
-            //setPosx(posx + 1) //Haciendo set del movimiento hacia la derecha.
+            //  setPosx(posx + 1) //Haciendo set del movimiento hacia la derecha.
 
-            //setPosx((oldPosx) => oldPosx + 1) //Haciendo set del movimiento hacia la derecha.
+            //  setPosx((oldPosx) => oldPosx + 1) //  Haciendo set del movimiento hacia la derecha.
 
-            //console.log('ww',maze[indexX][indexY],maze)
+            //  console.log('ww',maze[indexX][indexY],maze)
 
-            //Viendo si hay un espacio vacío.
+            //  Viendo si hay un espacio vacío.
             if(maze[indexX][indexY] === ' '){
-                setPosx((oldPosx) => oldPosx + 10) //Moviendo al personaje hacia la izquierda.
+                setPosx((oldPosx) => oldPosx + 10) // Moviendo al personaje hacia la izquierda.
             }else if(maze[indexX][indexY] === 'g'){
                 alert("Ganaste!")
             }
 
         }else if(e.key === "ArrowDown"){
            
-            //setPosx((oldPosx) => oldPosx - 1)
+            //  setPosx((oldPosx) => oldPosx - 1)
             
-            //Posición inicial del personaje.
+            //  Posición inicial del personaje.
 
-            //Menos uno de la posición en x del personaje. 
-            //Posición del personaje en x.
+            //  Menos uno de la posición en x del personaje. 
+            //  Posición del personaje en x.
             const lpixelx = posx
 
             console.log(lpixelx)
-            //Posición del personaje en y.
+            //  Posición del personaje en y.
             const lpixely = posy + 1
 
-            //Obteniendo la posición del personaje en el maze.
-            const indexX = Math.ceil((lpixelx/75)) //Posición en x.
-            const indexY = Math.ceil((lpixely/75)) //Posición en y.
+            //  Obteniendo la posición del personaje en el maze.
+            const indexX = Math.ceil((lpixelx/75)) // Posición en x.
+            const indexY = Math.ceil((lpixely/75)) // Posición en y.
 
-            ///console.log("Este es el maze cuando se presiona el botón de la izquierda: ", maze)
+            //  console.log("Este es el maze cuando se presiona el botón de la izquierda: ", maze)
 
-            //console.log(maze, indexX, indexY) //Posición del personaje en el maze.
-            //console.log(lpixelx)               //Posición inicial del personaje en x. 
+            //  console.log(maze, indexX, indexY) //  Posición del personaje en el maze.
+            //  console.log(lpixelx)               // Posición inicial del personaje en x. 
 
-            //console.log(lpixely)               //Posición del personaje ne y.
+            //  console.log(lpixely)               // Posición del personaje ne y.
               
-            //console.log("Datos del lab", maze)
+            //  console.log("Datos del lab", maze)
 
-            /*Detectando la flecha izquierda para poder mover al personaje*/
+            /* Detectando la flecha izquierda para poder mover al personaje */
             console.log("Abajo")
         
-            //setPosx(posx - 1) //Haciendo set del movimiento hacia la izquierda
+            //  setPosx(posx - 1) //Haciendo set del movimiento hacia la izquierda
 
-            //setPosx(indexX)
+            //  setPosx(indexX)
 
-            //setPosx((oldPosx) => oldPosx - 1) //Haciendo set del movimiento hacia la derecha.
+            //  setPosx((oldPosx) => oldPosx - 1) //Haciendo set del movimiento hacia la derecha.
             
             //Viendo si hay un espacio vacío.
             if(maze[indexX][indexY] === ' '){
-              setPosy((oldPosy) => oldPosy + 10) //Moviendo al personaje hacia la izquierda.
+              setPosy((oldPosy) => oldPosy + 10) // Moviendo al personaje hacia la izquierda.
             }else if(maze[indexX][indexY] === 'g'){
                 alert("Ganaste!")
             }
 
         }else if(e.key === "ArrowUp"){
             
-            //setPosx((oldPosx) => oldPosx - 1)
+            //  setPosx((oldPosx) => oldPosx - 1)
             
-            //Posición inicial del personaje.
+            //  Posición inicial del personaje.
 
-            //Menos uno de la posición en x del personaje. 
-            //Posición del personaje en x.
+            //  Menos uno de la posición en x del personaje. 
+            //  Posición del personaje en x.
             const lpixelx = posx
 
             console.log(lpixelx)
-            //Posición del personaje en y.
+            //  Posición del personaje en y.
             const lpixely = posy - 1
 
-            //Obteniendo la posición del personaje en el maze.
-            const indexX = Math.ceil((lpixelx/75)) //Posición en x.
-            const indexY = Math.ceil((lpixely/75)) //Posición en y.
+            //  Obteniendo la posición del personaje en el maze.
+            const indexX = Math.ceil((lpixelx/75)) // Posición en x.
+            const indexY = Math.ceil((lpixely/75)) // Posición en y.
 
-            ///console.log("Este es el maze cuando se presiona el botón de la izquierda: ", maze)
+            //  console.log("Este es el maze cuando se presiona el botón de la izquierda: ", maze)
 
-            //console.log(maze, indexX, indexY) //Posición del personaje en el maze.
-            //console.log(lpixelx)               //Posición inicial del personaje en x. 
+            //  console.log(maze, indexX, indexY) //Posición del personaje en el maze.
+            //  console.log(lpixelx)               //Posición inicial del personaje en x. 
 
-            //console.log(lpixely)               //Posición del personaje ne y.
+            //  console.log(lpixely)               //Posición del personaje ne y.
               
-            //console.log("Datos del lab", maze)
+            //  console.log("Datos del lab", maze)
 
-            /*Detectando la flecha izquierda para poder mover al personaje*/
+            /*  Detectando la flecha izquierda para poder mover al personaje  */
             console.log("Arriba")
         
-            //setPosx(posx - 1) //Haciendo set del movimiento hacia la izquierda
+            //  setPosx(posx - 1) //Haciendo set del movimiento hacia la izquierda
 
-            //setPosx(indexX)
+            //  setPosx(indexX)
 
-            //setPosx((oldPosx) => oldPosx - 1) //Haciendo set del movimiento hacia la derecha.
+            //  setPosx((oldPosx) => oldPosx - 1) //Haciendo set del movimiento hacia la derecha.
             
-            //Viendo si hay un espacio vacío.
+            //  Viendo si hay un espacio vacío.
             if(maze[indexX][indexY] === ' '){
-              setPosy((oldPosy) => oldPosy - 10) //Moviendo al personaje hacia la izquierda.
+              setPosy((oldPosy) => oldPosy - 10) //   Moviendo al personaje hacia la izquierda.
             }else if(maze[indexX][indexY] === 'g'){
                 alert("Ganaste")
             } 
@@ -230,18 +230,15 @@ function Laberinto () {
 
          return () => window.removeEventListener("keydown", mov)
     }, [mov])
-    //movimientos() //Llamando al método para detectar los movimientos.
+  //    movimientos() //Llamando al método para detectar los movimientos.
 
-    //window.addEventListener("keydown", mov)
+  //    window.addEventListener("keydown", mov)
 
-    //console.log("Laberinto luego del event listener: ",maze)
-
-
+  //    console.log("Laberinto luego del event listener: ",maze)
     return(
       <div className="Maze"> {/*Padre de todos los div's*/}
-          {/*Devolviendo un map que va a imprimir cada elemento del laberinto*/}
-          <h2>Ahora trata de llegar a la meta</h2> {/*Imprimiendo en pantalla un título h2 para indicar que se está imprimiendo el laberinto*/}
-          {/*Reproductor de audio*/}
+        {/* Devolviendo un map que va a imprimir cada elemento del laberinto */}
+        <h2>Ahora trata de llegar a la meta</h2> {/*Título*/}
           <div className='Mapa'> {/*Se mapea la matriz de laberinto y luego se mapea cada matriz que hay dentro de la matriz grande.*/}
               { /*Jalando todos los objetos de la matriz devuelta por el fetch*/
                   maze.map((elementos) => 
@@ -280,8 +277,8 @@ function Laberinto () {
                           }
                           return
                       })}
-                      </div>
-                  )}
+                    </div>)
+                      }
 
           </div>
       </div>
